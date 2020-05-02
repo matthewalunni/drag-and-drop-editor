910005897e1e11d5864d8aba4fe4031f1cc85832
matthewalunni <matthew.alunni@gmail.com>
2020-05-02 12:06:57 -0400

    Updates

---
+++ precache-manifest.a7f60b809bb75225f2dc4ac973c1fe68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5999c47711c5d555706001f13d5de7ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1575328b46eb97ffd20f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a7c0300c4070f7d3fa3c",
+    "url": "/static/css/main.509c6651.chunk.css"
+  },
+  {
+    "revision": "1575328b46eb97ffd20f",
+    "url": "/static/js/2.58cd8488.chunk.js"
+  },
+  {
+    "revision": "7b584e5702ef78dbabbc9d5f28996b56",
+    "url": "/static/js/2.58cd8488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c0300c4070f7d3fa3c",
+    "url": "/static/js/main.71f1c533.chunk.js"
+  },
+  {
+    "revision": "97165735905a9e32f6e9",
+    "url": "/static/js/runtime-main.09dc42ff.js"
+  }
+]);
