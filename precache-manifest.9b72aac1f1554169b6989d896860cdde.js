e821aeaa8d4962e87170e918470ad53ddff46063
matthewalunni <matthew.alunni@gmail.com>
2020-05-02 12:33:18 -0400

    Updates

---
+++ precache-manifest.9b72aac1f1554169b6989d896860cdde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09ad24f4dd3c7d13fa3157272fb3112",
+    "url": "/drag-and-drop-editor/index.html"
+  },
+  {
+    "revision": "d97930510e395c79464b",
+    "url": "/drag-and-drop-editor/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "774a9a5c3b8c448a8c41",
+    "url": "/drag-and-drop-editor/static/css/main.509c6651.chunk.css"
+  },
+  {
+    "revision": "d97930510e395c79464b",
+    "url": "/drag-and-drop-editor/static/js/2.ade284ac.chunk.js"
+  },
+  {
+    "revision": "7b584e5702ef78dbabbc9d5f28996b56",
+    "url": "/drag-and-drop-editor/static/js/2.ade284ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774a9a5c3b8c448a8c41",
+    "url": "/drag-and-drop-editor/static/js/main.7e16ee51.chunk.js"
+  },
+  {
+    "revision": "1d50759b3d643f2887a0",
+    "url": "/drag-and-drop-editor/static/js/runtime-main.c6704d14.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1ea8fc10e0319b53c3611c9c0f8caf0.js"
+  "/drag-and-drop-editor/precache-manifest.9b72aac1f1554169b6989d896860cdde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drag-and-drop-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
